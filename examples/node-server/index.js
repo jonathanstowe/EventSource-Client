c9840e0ae273e3a790b869ab13f63e347333a636
Jonathan Stowe <jns@gellyfish.co.uk>
2020-03-20 12:11:33 +0000

    Add Node.JS example server

---
+++ examples/node-server/index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const express = require('express')
+const SseStream = require('ssestream')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  console.log('new connection')
+  res.set('Transfer-Encoding', 'chunked');
+
+  const sseStream = new SseStream(req)
+  sseStream.pipe(res)
+  const pusher = setInterval(() => {
+    sseStream.write({
+      event: 'server-time',
+      data: new Date().toTimeString()
+    })
+  }, 1000)
+
+  res.set('Transfer-Encoding', 'chunked');
+  res.on('close', () => {
+    console.log('lost connection')
+    clearInterval(pusher)
+    sseStream.unpipe(res)
+  })
+})
+
+app.listen(7798, (err) => {
+  if (err) throw err
+  console.log('server ready on http://localhost:8080')
+})
